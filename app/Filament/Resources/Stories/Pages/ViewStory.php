06fa8d0989c8c08c9408de73148b31b6e2a7dae8
Ayann12 <tantowi818@gmail.com>
2026-09-08 14:17:42 +0700

    add fitur approve rejected and rework button

--- app/Filament/Resources/Stories/Pages/ViewStory.php
+++ app/Filament/Resources/Stories/Pages/ViewStory.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\Stories\Pages;
 
 use App\Filament\Resources\Stories\StoryResource;
 use Filament\Actions\EditAction;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewStory extends ViewRecord
@@ -14,6 +15,51 @@ class ViewStory extends ViewRecord
     {
         return [
             EditAction::make(),
+            Action::make('approve')
+                ->color('success')
+                ->label('Approve')
+                ->visible(fn() => auth()->user()->hasRole('Reviewer') && $this->record->status === 'in review' && $this->record->reviewer_id === auth()->id())
+                ->form([
+                    \Filament\Forms\Components\TextArea::make('feedback')
+                        ->label('feedback')
+                        ->required()
+                        ->rows(3)
+                        ->columnSpanFull(),
+                ])
+                ->action(function (array $data) {
+                    $this->record->update(['status' => 'Approved', 'feedback' => $data['feedback']]);
+                    // $this->notify('success', 'Story approved successfully');
+                }),
+            Action::make('cancel')
+                ->color('danger')
+                ->label('Cancel')
+                ->visible(fn() => auth()->user()->hasRole('Reviewer') && $this->record->status === 'in review' && $this->record->reviewer_id === auth()->id())
+                ->form([
+                    \Filament\Forms\Components\TextArea::make('feedback')
+                        ->label('feedback')
+                        ->required()
+                        ->rows(3)
+                        ->columnSpanFull(),
+                ])
+                ->action(function (array $data) {
+                    $this->record->update(['status' => 'Rejected', 'feedback' => $data['feedback']]);
+                    // $this->notify('success', 'Story cancel successfully');
+                }),
+
+            Action::make('rework')
+                ->color('info')
+                ->label('Rework')
+                ->visible(fn() => auth()->user()->hasRole('Reviewer') && $this->record->status === 'in review' && $this->record->reviewer_id === auth()->id())
+                ->form([
+                    \Filament\Forms\Components\TextArea::make('feedback')
+                        ->label('feedback')
+                        ->rows(3)
+                        ->columnSpanFull(),
+                ])
+                ->action(function (array $data) {
+                    $this->record->update(['status' => 'Rework', 'feedback' => $data['feedback']]);
+                    // $this->notify('success', 'Story rework successfully');
+                }),
         ];
     }
 }
